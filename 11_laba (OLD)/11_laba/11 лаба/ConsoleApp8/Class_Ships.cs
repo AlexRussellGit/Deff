6168fd2b2469259a81360e4c8b463ee3d7886479
Alex <79187801+AlexRussellGit@users.noreply.github.com>
2021-04-03 12:03:02 +0500

    Add files via upload

---
+++ 11_laba (OLD)/11_laba/11 лаба/ConsoleApp8/Class_Ships.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    /// <summary>
+    /// Класс "Корабли"
+    /// </summary>
+    public abstract class Class_Ships
+    {
+        protected string name;
+        protected int speed;
+        protected int displacement;
+
+        public string Name
+        {
+            set { name = value; }
+            get { return name; }
+        }
+
+        public int Speed
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    speed = 0;
+                }
+                else
+                {
+                    speed = value;
+                }
+            }
+            get { return speed; }
+        }
+
+        public int Displacement
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    displacement = 0;
+                }
+                else
+                {
+                    displacement = value;
+                }
+            }
+            get { return displacement; }
+        }
+
+        public Class_Ships()
+        {
+            Name = "Пустой";
+            Speed = 0;
+            Displacement = 0;
+        }
+
+        public Class_Ships(string Name, int Speed, int Displacement)
+        {
+            this.Name = Name;
+            this.Speed = Speed;
+            this.Displacement = Displacement;
+        }
+
+        public abstract void Show();
+
+        public override bool Equals(Object obj)
+        {
+            return Equals((Class_Ships)obj);
+        }
+        
+
+        public override int GetHashCode()
+        {
+            return 0;
+        }
+    }
+    /// <summary>
+    /// Класс "Парусник", созданный от класса "Корабли"
+    /// </summary>
+    public class Class_Sailboat_Ships : Class_Ships
+    {
+        protected int velum_number;
+        public int Velum_Number
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    velum_number = 0;
+                }
+                else
+                {
+                    velum_number = value;
+                }
+            }
+            get { return velum_number; }
+        }
+        public Class_Sailboat_Ships() : base()
+        {
+            Velum_Number = 0;
+        }
+        public Class_Sailboat_Ships(string Name, int Speed, int Displacement, int Velum_Numb) : base(Name, Speed, Displacement)
+        {
+            Velum_Number = Velum_Numb;
+        }
+        public override void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("ТИП - ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("ПАРУСНИК");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("a) Название судна: \"" + name + "\"\nб) Cкорость судна: " + speed + " (узлов)\nв) Водоизмещение судна: " + displacement + " (тонн)");
+            Console.WriteLine("г) Количество парусов на судне: " + velum_number + " (шт)");
+            Console.WriteLine();
+        }
+
+    }
+    /// <summary>
+    /// Класс "Корвет", созданный от класса "Парусник"
+    /// </summary>
+    public class Class_Corvette_Ships : Class_Sailboat_Ships
+    {
+        int cannon_amount;
+        static private int Numbers_Of_Class_Corvette_Ships = 0;
+        public int Cannon_Amount
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    cannon_amount = 0;
+                }
+                else
+                {
+                    cannon_amount = value;
+                }
+            }
+            get
+            {
+                return cannon_amount;
+            }
+        }
+        public Class_Corvette_Ships() : base()
+        {
+            Cannon_Amount = 0;
+            Numbers_Of_Class_Corvette_Ships++;
+
+        }
+        public Class_Corvette_Ships(string Name, int Speed, int Displacement, int Velum_Numb, int Cannon_Am) : base(Name, Speed, Displacement, Velum_Numb)
+        {
+            Cannon_Amount = Cannon_Am;
+            Numbers_Of_Class_Corvette_Ships++;
+
+        }
+        public override void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("ТИП - ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("ПАРУСНИК");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("ПОДТИП - ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("КОРВЕТ");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("a) Название судна: \"" + name + "\"\nб) Cкорость судна: " + speed + " (узлов)\nв) Водоизмещение судна: " + displacement + " (тонн)");
+            Console.WriteLine("г) Количество парусов на судне: " + velum_number + " (шт)");
+            Console.WriteLine("д) Количество огнестрельных орудий на судне: " + cannon_amount + " (шт)");
+            Console.WriteLine();
+        }
+        static public void Numb_Of_Corvette()
+        {
+            Console.WriteLine("Количествво созданных объектов типа \"Корвет\" за данный сеанс работы программы: " + Numbers_Of_Class_Corvette_Ships);
+        }
+    }
+    /// <summary>
+    /// Класс "Пароход", созданный от класса "Корабли"
+    /// </summary>
+    public class Class_Steamboat_Ships : Class_Ships, ICloneable
+    {
+        int engine_capacity;
+        public int Engine_Capacity
+        {
+            set
+            {
+                if (value < 0)
+                {
+                    engine_capacity = 0;
+                }
+                else
+                {
+                    engine_capacity = value;
+                }
+            }
+            get { return engine_capacity; }
+        }
+        public Class_Steamboat_Ships() : base()
+        {
+            Engine_Capacity = 0;
+        }
+        public Class_Steamboat_Ships(string Name, int Speed, int Displacement, int Engine_Cap) : base(Name, Speed, Displacement)
+        {
+            Engine_Capacity = Engine_Cap;
+        }
+        public override void Show()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.Write("ТИП - ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("ПАРОХОД");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("a) Название судна: \"" + name + "\"\nб) Cкорость судна: " + speed + " (узлов)\nв) Водоизмещение судна: " + displacement + " (тонн)");
+            Console.WriteLine("г) Мощность дивгателя: " + engine_capacity + " (лс)");
+            Console.WriteLine();
+        }
+        public object Clone()
+        {
+            return new Class_Steamboat_Ships("Клон " + this.name, this.speed, this.displacement, this.engine_capacity);
+        }
+
+    }
+    public class Class_Ships_Enum : IEnumerator
+    {
+        public Class_Ships[] _people;
+
+        // Enumerators are positioned before the first element
+        // until the first MoveNext() call.
+        int position = -1;
+
+        public Class_Ships_Enum(Class_Ships[] list)
+        {
+            _people = list;
+        }
+
+        public bool MoveNext()
+        {
+            position++;
+            return (position < _people.Length);
+        }
+
+        public void Reset()
+        {
+            position = -1;
+        }
+
+        object IEnumerator.Current
+        {
+            get
+            {
+                return Current;
+            }
+        }
+
+        public Class_Ships Current
+        {
+            get
+            {
+                try
+                {
+                    return _people[position];
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    throw new InvalidOperationException();
+                }
+            }
+        }
+    }
+}
---
+++ 11_laba (OLD)/11_laba/11 лаба/ConsoleApp8/MySortedDictionary.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    public class MySortedDictionary<K, T> : IEnumerable
+    {
+        private SortedList<K, T> sl;
+        private int capacity;
+        public MySortedDictionary()
+        {
+            sl = new SortedList<K, T>();
+            capacity = 0;
+        }
+        public MySortedDictionary(int c)
+        {
+            sl = new SortedList<K, T>();
+            capacity = c;
+        }
+        public MySortedDictionary(IDictionary<K, T> d)
+        {
+            sl = new SortedList<K, T>(d);
+            capacity = d.Count();
+        }
+        public IEnumerator GetEnumerator()
+        {
+            return Values.GetEnumerator();
+        }
+        public int Capacity
+        {
+            get { return capacity; }
+            set
+            {
+                if (value < 0) value = 0;
+                capacity = value;
+            }
+        }
+        public int Count
+        {
+            get { return sl.Count; }
+        }
+        public IList<K> Keys
+        {
+            get { return sl.Keys; }
+        }
+        public IList<T> Values
+        {
+            get { return sl.Values; }
+            set { Values = value; }
+        }
+        public bool ContainsKey(object key)
+        {
+            return sl.ContainsKey((K)key);
+        }
+        public bool ContainsValue(object value)
+        {
+
+            return sl.ContainsValue((T)value);
+        }
+        public void Add(K key, T value)
+        {
+            if (ContainsKey(key) == true)
+            {
+                Console.WriteLine("Запись с таким именем уже существует", Console.ForegroundColor = ConsoleColor.Red);
+                Console.WriteLine();
+                Console.ResetColor();
+                return;
+            }
+            sl.Add(key, value);
+            if (Count > Capacity)
+                Capacity++;
+        }
+
+        public void Clear()
+        {
+            Console.WriteLine("Очищаем словарь");
+            sl.Clear();
+            Console.WriteLine("Словарь очищен");
+            Console.WriteLine();
+        }
+        public void Remove(object value)
+        {
+            MySortedDictionary<K, T> ms = new MySortedDictionary<K, T>(sl);
+            int index = ms.IndexOfValue(value);
+            K key = ms.GetKey(index);
+            sl.Remove(key);
+        }
+        public void RemoveAt(int index)
+        {
+            sl.RemoveAt(index);
+        }
+        public void RemoveAtKey(K Key)
+        {
+            sl.Remove(Key);
+        }
+        public MySortedDictionary<K, T> Clone()
+        {
+            Console.WriteLine("Клонируем словарь");
+            MySortedDictionary<K, T> clone = new MySortedDictionary<K, T>(sl) { Capacity = Capacity };
+            Console.WriteLine("Клонирование завершено");
+            Console.WriteLine();
+            return clone;
+        }
+        public T GetByIndex(int index)
+        {
+            if (index > Count - 1)
+            {
+                Console.WriteLine("Записи под индексом " + index + " нет в словаре", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+                return default(T);
+            }
+            SortedList s = new SortedList(sl);
+            T value = (T)s.GetByIndex(index);
+            return value;
+        }
+
+        public K GetKey(int index)
+        {
+            if (index > Count - 1)
+            {
+                Console.WriteLine("Ключа с таким индексом нет в словаре", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+                return default(K);
+            }
+            SortedList s = new SortedList(sl);
+            K key = (K)s.GetKey(index);
+            return key;
+        }
+        public int IndexOfKey(object key)
+        {
+            return sl.IndexOfKey((K)key);
+        }
+        public int IndexOfValue(object value)
+        {
+
+            return sl.IndexOfValue((T)value);
+        }
+        public void SetByIndex(int index, object value)
+        {
+            if (index > Count - 1)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Ключа с таким индексом нет в словаре", Console.ForegroundColor = ConsoleColor.Red);
+                Console.ResetColor();
+            }
+            else
+            {
+                MySortedDictionary<K, T> ms = new MySortedDictionary<K, T>(sl);
+                K key = ms.GetKey(index);
+                sl.RemoveAt(index);
+                sl.Add(key, (T)value);
+            }
+        }
+        public void Show()
+        {
+            Console.WriteLine("Словарь:");
+            Console.WriteLine();
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine("{0})", GetKey(i));
+                if (GetByIndex(i).GetType() == typeof(Class_Corvette_Ships) || GetByIndex(i).GetType() == typeof(Class_Sailboat_Ships) || GetByIndex(i).GetType() == typeof(Class_Steamboat_Ships))
+                {
+                    object obj = GetByIndex(i);
+                    Class_Ships ship = (Class_Ships)obj;
+                    ship.Show();
+                }
+                else
+                    GetByIndex(i);
+            }
+
+        }
+    }
+}
+
