ce2456e6806dea01a2bfa803f7c339ede57a6317
Alex <79187801+AlexRussellGit@users.noreply.github.com>
2021-04-07 06:57:44 +0500

    Add files via upload

---
+++ 11_laba/11 лаба/ConsoleApp8/ColorTextOut.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class ColorTextOut
+    {
+        public static void DarkRedText(string str) // ТЁМНО-КРАСНЫЙ ВЫВОД
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(str);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void RedText(string str) // КРАСНВЫЙ ВЫВОД
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(str);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void DarkCyanText(string str) // ТЁМНО-ЦИАНОВЫЙ ВЫВОД
+        {
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.Write(str);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void CyanText(string str) // ЦИАНОВЫЙ ВЫВОД
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write(str);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void YellowText(string str) // ЖЁЛТЫЙ ВЫВОД
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write(str);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+        public static void GreenText(string str) // ЗЕЛЁНЫЙ ВЫВОД
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(str);
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+        }
+    }
+}
